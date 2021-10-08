a8efb0732d0fb92bfad0638f2b7bcc32720f97cd
Dmitri Trofimov <dmitri.trofimov@prime-initiative.com>
2021-10-08 21:43:06 +0300

    updated packages

--- tools/data_retriever/src/utils/data-provider.ts
+++ tools/data_retriever/src/utils/data-provider.ts
@@ -3,23 +3,27 @@ import parseCsv from "csv-parse/lib/sync";
 
 export abstract class DataProvider {
     protected async getParsedData(url: string): Promise<string[][]> {
-        const rawText = await this.getRawTextData(url);
+        const rawText = await this._getRawTextData(url);
         const result = parseCsv(rawText) as string[][];
 
         return result;
     }
 
-    private async getRawTextData(url: string): Promise<string> {
+    private async _getRawTextData(url: string): Promise<string> {
         try {
             const response = await axios.get<string>(url);
 
             return response.data;
         } catch (error) {
-            if (!error.isAxiosError || !error.response?.status || !error.response?.statusText) {
-                throw error;
+            if (axios.isAxiosError(error)) {
+                if (!error.response) {
+                    throw new Error(`Faied to retrieve airport data: '${error.message}'.`);
+                }
+                console.log(error);
+                throw new Error(`Faied to retrieve airport data: '${error.response?.status} - ${error.response?.statusText}'.`);
             }
 
-            throw new Error(`Faied to retrieve airport data: '${error.response.status} - ${error.response.statusText}`);
+            throw error;
         }
     }
 }
